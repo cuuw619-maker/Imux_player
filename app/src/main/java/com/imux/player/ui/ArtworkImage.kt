91f5b1e87bd79d2fec2302331c11f75aee2f56f7
cuuw619-maker <cuuw619@gmail.com>
2026-09-20 20:14:38 +0300

    Fix artwork image modifier import

--- app/src/main/java/com/imux/player/ui/ArtworkImage.kt
+++ app/src/main/java/com/imux/player/ui/ArtworkImage.kt
@@ -3,6 +3,7 @@ package com.imux.player.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MusicNote
 import androidx.compose.material3.Icon
